a18571dcb74ab8a5a0513d0c6d6e3adf804ae748
vmorin <virginiemorin@yahoo.com>
2024-04-12 18:12:19 +0200

    update 12 avril

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -12,10 +12,13 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
 
 #[ORM\HasLifecycleCallbacks]
 #[UniqueEntity('slug', message:'Ce slug existe déjà.')]
 #[ORM\Entity(repositoryClass: PostRepository::class)]
+#[Vich\Uploadable]
 class Post
 {
     const STATES = ['DRAFT', 'PUBLISHED'];
@@ -23,6 +26,17 @@ class Post
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
+
+    #[Vich\UploadableField(mapping: 'post', fileNameProperty: 'imageName', size: 'imageSize')]
+    private ?File $imageFile = null;
+
+
+    #[ORM\Column(nullable: true)]
+    private ?string $imageName = null;
+
+
+    #[ORM\Column(nullable: true)]
+    private ?int $imageSize = null;
 
     #[ORM\Column(length: 255, unique:true)]
     #[Assert\NotBlank(message:'Le titre ne peut pas être vide.') ]
@@ -56,6 +70,44 @@ class Post
     #[ORM\PreUpdate]
     public function preUpdate(){
         $this->updatedAt = new \DateTimeImmutable();
+    }
+    /**
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setImageSize(?int $imageSize): void
+    {
+        $this->imageSize = $imageSize;
+    }
+
+    public function getImageSize(): ?int
+    {
+        return $this->imageSize;
     }
 
 
